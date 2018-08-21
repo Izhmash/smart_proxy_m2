cfd99df589539bf85625db7a879a152c2db75bbf
Ian Ballou <ianballou67@gmail.com>
2018-08-21 18:38:16 -0400

    added tests and default config options to get the tests to work

--- lib/smart_proxy_m2/m2.rb
+++ lib/smart_proxy_m2/m2.rb
@@ -4,7 +4,11 @@ module Proxy::M2
   class Plugin < ::Proxy::Plugin
     plugin 'm2', Proxy::M2::VERSION
 
-    default_settings :hello_greeting => 'O hai!'
+    default_settings :test_msg => 'test'
+    default_settings :bmi_endpoint => 'http://localhost'
+    default_settings :bmi_username => 'user'
+    default_settings :bmi_password => 'pass'
+    default_settings :bmi_project => 'bmi_infra'
 
     http_rackup_path File.expand_path('m2_http_config.ru', File.expand_path('../', __FILE__))
     https_rackup_path File.expand_path('m2_http_config.ru', File.expand_path('../', __FILE__))
--- test/m2_api_test.rb
+++ test/m2_api_test.rb
@@ -13,9 +13,118 @@ class M2ApiTest < Test::Unit::TestCase
     Proxy::M2::Api.new
   end
 
-  def test_returns_test_greeting
+  def test_test
     Proxy::M2::Plugin.load_test_settings('test_msg' => 'test')
     assert get('/test').body == 'test'
   end
 
+  def test_image_list
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:post, "http://localhost/list_images/"). with( body: {"project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_image", headers: {})
+
+    get '/image_list?project=bmi_infra'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("test_image", last_response.body)
+
+  end
+
+  def test_image_list_default_project
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:post, "http://localhost/list_images/"). with( body: {"project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_image", headers: {})
+
+    get '/image_list'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("test_image", last_response.body)
+
+  end
+
+  def test_get_target
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:put, "http://localhost/create_disk"). with( body: {"disk_name"=>"test_disk", "img"=>"test_image", "project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_target", headers: {})
+
+    get '/iscsi_target?project=bmi_infra&disk=test_disk&image=test_image'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("iscsi:localhost:::1:test_target", last_response.body)
+
+  end
+
+  def test_get_target_default_project
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:put, "http://localhost/create_disk"). with( body: {"disk_name"=>"test_disk", "img"=>"test_image", "project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_target", headers: {})
+
+    get '/iscsi_target?disk=test_disk&image=test_image'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("iscsi:localhost:::1:test_target", last_response.body)
+
+  end
+
+  def test_delete_target
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:delete, "http://localhost/delete_disk"). with( body: {"disk_name"=>"test_disk", "project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "Success", headers: {})
+
+    delete '/iscsi_target?project=bmi_infra&disk=test_disk'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("Success", last_response.body)
+
+  end
+
+  def test_delete_target_default_project
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:delete, "http://localhost/delete_disk"). with( body: {"disk_name"=>"test_disk", "project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "Success", headers: {})
+
+    delete '/iscsi_target?disk=test_disk'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("Success", last_response.body)
+
+  end
+
+  def test_get_snapshots
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:post, "http://localhost/list_snapshots/"). with( body: {"project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_snapshot", headers: {})
+
+    get '/snapshot_list?project=bmi_infra'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("test_snapshot", last_response.body)
+
+  end
+
+  def test_get_snapshots_default_project
+
+    Proxy::M2::Plugin.load_test_settings('bmi_username' => 'user')
+    Proxy::M2::Plugin.load_test_settings('bmi_password' => 'pass')
+    Proxy::M2::Plugin.load_test_settings('bmi_project' => 'bmi_infra')
+
+    stub_request(:post, "http://localhost/list_snapshots/"). with( body: {"project"=>"bmi_infra"}, headers: { 'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Basic dXNlcjpwYXNz', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'localhost', 'User-Agent'=>'Ruby' }). to_return(status: 200, body: "test_snapshot", headers: {})
+
+    get '/snapshot_list'
+    assert last_response.ok?, "Last response was not ok: #{last_response.body}"
+    assert_equal("test_snapshot", last_response.body)
+
+  end
 end
